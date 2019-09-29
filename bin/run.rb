a0c03b4bea6ec1b1d2a6421a3e7be838bb7bb3ee
Alouiscious Media <info@reneesoul.com>
2019-09-28 22:15:46 -0700

    git even post delta pokemon.rd

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,8 @@
-require_relative "environment"
+#!usr/bin/env ruby
 
+
+require_relative "environment"
 Scraper.new(@db).scrape
 
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
-
 # test out your code here!
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def self.save(name, type, db)
+    # if self.id
+    #   self.update
+    # else
+
+      sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+      SQL
+
+      db.execute(sql, name, type)
+      id = db.execute("SELECT last_insert_rowid() FROM pokemon;")[0][0]
+    # end
+
+  end
+
+  def self.find(num, db)
+    # finds a pokemon from the database by an id number passed in
+    # returns a new Pokemon object
+    query_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?;", num).flatten
+    Pokemon.new(id: query_pokemon[0], name: query_pokemon[1], type: query_pokemon[2], db: db)
+  end
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+
+  end
+
 end
